216e4e1c1c30e46c4730a9f03d595dd7c9103dd0
Memphes <Memphes20007@yandex.ru>
2023-01-07 13:49:54 +0300

    AddUniRX

--- Assets/DYakubenko/Scripts/DataSource.cs
+++ Assets/DYakubenko/Scripts/DataSource.cs
@@ -1,7 +1,39 @@
-﻿namespace DYakubenko
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DYakubenko
 {
-    public class DataSource
+    public class DataSource : MonoBehaviour
+
     {
+        [SerializeField] private int money = 0;
+        
+        [SerializeField] [Range(0, 100)] private int health = 100;
+        [SerializeField] [Range(0, 100)] private int hunger = 0;
+        [SerializeField] [Range(0, 100)] private int condition = 50;
+        [SerializeField] [Range(0, 100)] private int status = 0;
+        
+        [SerializeField] private int day = 0;
+
+        
+        
+        private const string KeyMoney = "moneyValue";
+        private const string KeyHealth = "healthValue";
+        private const string KeyHunger = "hungerValue";
+        private const string KeyCondition = "conditionValue";
+        private const string KeyStatus = "statusValue";
+        private const string KeyDay = "dayValue";
+
+        private void Awake()
+        {
+            money = PlayerPrefs.GetInt(KeyMoney, 0);
+            health = PlayerPrefs.GetInt(KeyHealth, 100);
+            hunger = PlayerPrefs.GetInt(KeyHunger, 0);
+            condition = PlayerPrefs.GetInt(KeyCondition, 50);
+            status = PlayerPrefs.GetInt(KeyStatus, 0);
+            day = PlayerPrefs.GetInt(KeyDay, 0);
+        }
         
     }
 }
